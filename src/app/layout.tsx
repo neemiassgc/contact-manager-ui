cec40b850c4912162cc5af72f1d20b2d25303147
Neemias Santos <neemiassgc@outlook.com>
2024-04-28 23:49:24 -0300

    new: settings and intial files

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Contact Manager",
+  description: "Web UI to interact with an API that manages contacts",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
