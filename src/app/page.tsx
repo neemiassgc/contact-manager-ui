667dc50673c96bec2fb509a06b15fa412367a7f3
Neemias Santos <neemiassgc@outlook.com>
2024-05-04 09:17:23 -0300

    feat: add a pagination component and implement its logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { InputAdornment, Button, Box, Avatar, OutlinedInput, Checkbox } from '@mui/material';
+import { InputAdornment, Button, Box, Avatar, OutlinedInput, Checkbox, Pagination } from '@mui/material';
 import { ListItem, ListItemAvatar, ListItemButton, ListItemText } from '@mui/material';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import SearchIcon from '@mui/icons-material/Search';
@@ -41,6 +41,11 @@ function ContactActionHeader() {
 
 function ContactBoard() {
   const [indexSet, setIndexSet] = useState([false, false, false, false]);
+  const [page, setPage] = useState(1);
+
+  const handlePagination: (event: React.ChangeEvent<unknown>, value: number) => void = (_, value) => {
+    setPage(value);
+  }
 
   const handleClick: (index: any) => void = index => {
     const newSet = [...indexSet];
@@ -49,13 +54,24 @@ function ContactBoard() {
   }
 
   return (
-    <Box className="w-full bg-white rounded-lg border-2">
-      {generateRow(["Tom", "Jerry", "Mickey", "Rose"], handleClick, indexSet)}
-    </Box>
+    <>
+      <Box className="w-full bg-white rounded-lg border-2">
+        {generateList(getData(3, page), handleClick, indexSet)}
+      </Box>
+      <Box className="p-2">
+        <Pagination className="w-fit mx-auto" count={4} page={page} onChange={handlePagination} size="large" />
+      </Box>
+    </>
   )
 }
 
-function generateRow(contactNames: string[], handleClick: (event: any) => void, indexSet: boolean[]) {
+function getData(size: number, page: number): string[] {
+  const names: string[] = ["Tom", "Jerry", "Mickey", "Rose", "Gary", "Tyrel", "Gulia", "Roger", "Anna", "Steven", "Bob", "Richard"];
+  const viewStart: number = size * page - size;
+  return names.slice(viewStart === 0 ? 0 : viewStart, size * page);
+}
+
+function generateList(contactNames: string[], handleClick: (event: any) => void, indexSet: boolean[]) {
   const elements = [];
   for (let i = 0; i < contactNames.length; i++) {
     elements.push(
