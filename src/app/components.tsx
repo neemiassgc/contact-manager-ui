9d36962f8846017658080ec9a1aad7e0b98a6501
Neemias Santos <neemiassgc@outlook.com>
2024-05-08 12:16:34 -0300

    feat: create a componenet named AddressBoard

--- src/app/components.tsx
+++ src/app/components.tsx
@@ -5,16 +5,20 @@ import {
   Avatar,
   Box,
   Button, Dialog, DialogActions, DialogContent,
-  DialogTitle, IconButton, List, ListItem, ListSubheader,
+  DialogTitle, IconButton, InputAdornment, List, ListItem, ListSubheader,
+  TextField,
+  ToggleButton,
+  ToggleButtonGroup,
 } from "@mui/material";
 import CancelIcon from '@mui/icons-material/Cancel';
-import BadgeIcon from '@mui/icons-material/Badge';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
 import HomeIcon from '@mui/icons-material/Home';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Contact } from "./net";
+import { Contact, Address, AddressType } from "./net";
+import { useState } from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
 
 export function ContactView({open, contact, handleClose}: {open: boolean, contact: Contact, handleClose: () => void}) {
   return (
@@ -69,20 +73,7 @@ export function ContactView({open, contact, handleClose}: {open: boolean, contac
            <HomeIcon className="mr-2"/> Address
           </AccordionSummary>
           <AccordionDetails>
-            {
-              toKeys(contact.addresses).map(item => {
-                return <>
-                  <ListSubheader>{item}</ListSubheader>
-                  <ListItem divider secondaryAction={<IconButton edge="end"><DeleteIcon/></IconButton>}>
-                    {
-                      ["street", "city", "state", "country", "zipcode"]
-                        .map(value => contact.addresses[item][value])
-                        .join("; ")
-                    }
-                  </ListItem>
-                </>
-              })
-            }
+            <AddressBoard addresses={contact.addresses}/>
           </AccordionDetails>
         </Accordion>
       </DialogContent>
@@ -94,6 +85,54 @@ export function ContactView({open, contact, handleClose}: {open: boolean, contac
         >Close</Button>
       </DialogActions>
     </Dialog>
+  )
+}
+
+function AddressBoard({ addresses }: { addresses: AddressType }) {
+  const [alignment, setAlignment] = useState(0);
+
+  function handleAlignment(_: any, value: number): void {
+    setAlignment(value);
+  }
+
+  const addressKeys: string[] = toKeys(addresses);
+
+  return (
+    <>
+      <Box className="w-fit mx-auto mb-3">
+        <ToggleButtonGroup className="mx-auto" value={alignment} exclusive onChange={handleAlignment} size="small">
+          {
+            addressKeys.map((item, index) => <ToggleButton key={index} value={index}>{item}</ToggleButton>)
+          }
+        </ToggleButtonGroup>
+      </Box>
+      <Box>
+        {
+          addressKeys.map((key, index) => {
+              return <Box key={index} className={"flex gap-2 flex-wrap w-full "+(index === alignment ? "block" : "hidden")}>
+                {
+                  toKeys(addresses[key]).map((prop, index) => {
+                    return <TextField
+                      key={index}
+                      InputProps={{
+                        endAdornment: (
+                        <IconButton>
+                          <EditNoteIcon/>
+                        </IconButton>
+                        ),
+                        disableUnderline: true
+                      }}
+                      inputProps={{readOnly: true}}
+                      variant="filled"
+                      label={prop}
+                      value={addresses[key][prop]}/>
+                  })
+                }
+              </Box>
+          })
+        }
+      </Box>
+    </>
   )
 }
 
