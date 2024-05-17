c846bea8d263828b1793f765afbaba2f89627aee
Neemias Santos <neemiassgc@outlook.com>
2024-05-17 12:02:28 -0300

    refactor: modify the header of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Avatar, Box, Button } from "@mui/material";
+import { Avatar, Box, Button, Chip, IconButton, Typography } from "@mui/material";
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 import LogoutIcon from '@mui/icons-material/Logout';
 
@@ -29,20 +29,16 @@ export default function RootLayout({
 
 function Header() {
   return (
-    <header className="bg-white w-full text-right p-3 flex justify-between border-b-2">
-      <Box className="ml-5 border p-2 rounded-lg">
-        <ContactPhoneIcon className="text-4xl"/>
-        <span className="ml-2 align-middle text-lg">Contact Manager</span>
+    <header className="bg-white w-full text-right p-3 flex justify-between items-center border-b-2">
+      <Box className="ml-5">
+        <ContactPhoneIcon fontSize="large"/>
+        <span className="ml-2 align-middle text-md">Contact Manager</span>
       </Box>
-      <Box className="flex items-center mr-8 gap-x-6">
-        <Box className="align-center flex items-center gap-x-2">
-          <Avatar className="">R</Avatar>
-          <span className='align-middle'>Ronald</span>
+      <Box className="flex items-center mr-10">
+        <Box className="flex items-center gap-2">
+          <Avatar>R</Avatar>
+          <Typography>Ronald</Typography>
         </Box>
-        <Button
-          variant="outlined" size="small" color="secondary"
-          endIcon={<LogoutIcon/>}
-        >Log out</Button>
       </Box>
     </header>
   )
