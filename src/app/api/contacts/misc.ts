fb3c0b1fedf63dd9bafb899dc9bc02ec1c3c8f07
Neemias Santos <neemiassgc@outlook.com>
2024-06-17 11:20:34 -0300

    chore: abstract code of error handling into a new function named safe

--- src/app/api/contacts/misc.ts
+++ src/app/api/contacts/misc.ts
@@ -1,4 +1,5 @@
-import { getAccessToken } from "@auth0/nextjs-auth0"
+import { getAccessToken, AccessTokenError } from "@auth0/nextjs-auth0"
+import { NextResponse } from "next/server";
 
 export async function authorizedFetch(url: string, inputHeaders?: object) {
   const token = await getAccessToken();
@@ -11,4 +12,22 @@ export async function authorizedFetch(url: string, inputHeaders?: object) {
     return fetch(url, { headers });
   }
   throw new Error("Unauthorized");
+}
+
+export async function safe(run: () => Promise<NextResponse>): Promise<NextResponse> {
+  try {
+    return await run();
+  }
+  catch(error) {
+    console.error(error)
+    if (error instanceof Error && error.message === "Unauthorized")
+      return new NextResponse(error.message, { status: 401 });
+    if (error instanceof AccessTokenError) {
+      return new NextResponse(error.message, { status: 400 });
+    }
+    if (error instanceof TypeError) {
+      return new NextResponse(error.message, { status: 400 });
+    }
+    return new NextResponse((error as { message: string}).message, { status: 500 });
+  }
 }
--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -1,23 +1,13 @@
-import { AccessTokenError } from "@auth0/nextjs-auth0"
 import { NextResponse } from "next/server";
-import { authorizedFetch } from "./misc";
+import { authorizedFetch, safe } from "./misc";
 
 const resourceServerUri = "http://localhost:8080/api"
 
 export async function GET() {
-  try {
+  return safe(async () => {
     const fetchRequest = await authorizedFetch(resourceServerUri+"/contacts");
     if (!fetchRequest.ok)
       return new NextResponse(await fetchRequest.text(), { status: fetchRequest.status })
     return NextResponse.json(await fetchRequest.json());
-  }
-  catch(error) {
-    console.error(error)
-    if (error instanceof AccessTokenError) {
-      return new NextResponse(error.message, { status: 400 });
-    }
-    else if (error instanceof Error && error.message === "Unauthorized")
-      return new NextResponse(error.message, { status: 401 });
-    return new NextResponse("Something went wrong with the server", { status: 400 });
-  }
+  });
 }
