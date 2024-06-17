1bc354c206464a91fcdbd6d315f2eb486ebe037b
Neemias Santos <neemiassgc@outlook.com>
2024-06-17 11:10:54 -0300

    refactor: fix a mistake

--- src/app/api/contacts/misc.ts
+++ src/app/api/contacts/misc.ts
@@ -1,12 +1,14 @@
-import { getAccessToken, AccessTokenError } from "@auth0/nextjs-auth0"
+import { getAccessToken } from "@auth0/nextjs-auth0"
 
 export async function authorizedFetch(url: string, inputHeaders?: object) {
   const token = await getAccessToken();
-  const headers = {
-    authorization: `Bearer ${token}`,
-    accept: "*/*",
-    ...inputHeaders
+  if (token.accessToken) {
+    const headers = {
+      authorization: `Bearer ${token.accessToken}`,
+      accept: "*/*",
+      ...inputHeaders
+    }
+    return fetch(url, { headers });
   }
-  if (token.accessToken) return fetch(url, { headers });
   throw new Error("Unauthorized");
 }
