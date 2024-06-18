42f1640757b201d9ca2f84c3e54f6af6138c8cf6
Neemias Santos <neemiassgc@outlook.com>
2024-06-18 01:07:23 -0300

    feat: add POST handling for the /api/contacts endpoint

--- src/app/api/contacts/route.ts
+++ src/app/api/contacts/route.ts
@@ -1,13 +1,27 @@
 import { NextResponse } from "next/server";
 import { authorizedFetch, safe } from "./misc";
 
-const resourceServerUri = "http://localhost:8080/api"
+const resourceServerUri = "http://localhost:8080/api/contacts"
 
 export async function GET() {
   return safe(async () => {
-    const fetchRequest = await authorizedFetch(resourceServerUri+"/contacts");
+    const fetchRequest = await authorizedFetch(resourceServerUri);
     if (!fetchRequest.ok)
       return new NextResponse(await fetchRequest.text(), { status: fetchRequest.status })
     return NextResponse.json(await fetchRequest.json());
+  });
+}
+
+export async function POST(request: Request) {
+  return safe(async () => {
+    const settings: object = {
+      method: "POST",
+      body: request.body,
+      headers: {
+        contentType: "application/json"
+      }
+    }
+    const fetchRequest = await authorizedFetch(resourceServerUri, settings);
+    return new NextResponse(null, { status: fetchRequest.status });
   });
 }
