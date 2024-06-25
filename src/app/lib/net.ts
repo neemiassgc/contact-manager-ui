e9b7355022bec840e98cad54f3587fdc438de3b9
Neemias Santos <neemiassgc@outlook.com>
2024-06-25 00:38:03 -0300

    feat: create a new function named requester

--- src/app/lib/net.ts
+++ src/app/lib/net.ts
@@ -1,3 +1,16 @@
+import { Contact, ErrorType } from "./types";
+
 function getOrigin(): string {
   return window.location.origin;
+}
+
+async function requester(url: string, options?: object): Promise<Contact[] | string> {
+  console.log(url)
+  const request = await fetch(url, options);
+
+  if (!request.ok) {
+    throw new ErrorType(await request.text(), request.status);
+  }
+
+  return await request.json();
 }
