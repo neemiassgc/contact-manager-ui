c4f170afdd9598fe80f7c5fc267b058f4ef48283
Neemias Santos <neemiassgc@outlook.com>
2024-06-25 01:11:40 -0300

    chore: create a function named saveLocalContacts for storing contacts locally

--- src/app/lib/storage.ts
+++ src/app/lib/storage.ts
@@ -1,5 +1,10 @@
 import { Contact } from "./types"
 
+
+export function saveLocalContacts(contacts: Contact[]) {
+	localStorage.setItem("contacts", JSON.stringify(contacts));
+}
+
 export function getSelectedContact(): Contact | null {
 	const selectedContact: string | null = localStorage.getItem("selectedContact");
 	if (selectedContact) return JSON.parse(selectedContact);
