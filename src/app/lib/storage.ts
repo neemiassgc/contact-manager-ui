6ada2c26fd22178bd7c3d3b36af7305a06abb21d
Neemias Santos <neemiassgc@outlook.com>
2024-06-25 01:14:11 -0300

    chore: create a function that gets contacts from the local storage named getLocalContacts

--- src/app/lib/storage.ts
+++ src/app/lib/storage.ts
@@ -5,6 +5,12 @@ export function saveLocalContacts(contacts: Contact[]) {
 	localStorage.setItem("contacts", JSON.stringify(contacts));
 }
 
+export function getLocalContacts(): Contact[] | null {
+	const contacts: string | null = localStorage.getItem("contacts");
+	if (!contacts) return null;
+	return JSON.parse(contacts);
+}
+
 export function getSelectedContact(): Contact | null {
 	const selectedContact: string | null = localStorage.getItem("selectedContact");
 	if (selectedContact) return JSON.parse(selectedContact);
