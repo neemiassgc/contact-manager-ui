e6c4861cb1d4aef92ac0c9ace7b6fc88513de071
Neemias Santos <neemiassgc@outlook.com>
2024-07-03 12:13:02 -0300

    chore: create a function that clears the local cache named clearLocalContacts

--- src/app/lib/storage.ts
+++ src/app/lib/storage.ts
@@ -1,6 +1,5 @@
 import { Contact } from "./types"
 
-
 export function saveLocalContacts(contacts: Contact[]) {
 	localStorage.setItem("contacts", JSON.stringify(contacts));
 }
@@ -9,6 +8,10 @@ export function getLocalContacts(): Contact[] | null {
 	const contacts: string | null = localStorage.getItem("contacts");
 	if (!contacts) return null;
 	return JSON.parse(contacts);
+}
+
+export function clearLocalContacts(): void {
+	localStorage.removeItem("contacts");
 }
 
 export function getSelectedContact(): Contact | null {
