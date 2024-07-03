2018fd473055289e1be172571b10e500f2894e77
Neemias Santos <neemiassgc@outlook.com>
2024-07-03 12:45:53 -0300

    feat: create a set of functions that manage newly created contacts locally

--- src/app/lib/storage.ts
+++ src/app/lib/storage.ts
@@ -23,3 +23,32 @@ export function getSelectedContact(): Contact | null {
 export function setSelectedContact(contact: Contact) {
   localStorage.setItem("selectedContact", JSON.stringify(contact));
 }
+
+export function addUnseenContactName(contactName: string) {
+	const unseenContactNames: string[] = getAllUnseenContactNames();
+	unseenContactNames.push(contactName);
+	setUnseenContactNames(unseenContactNames);
+}
+
+export function getAllUnseenContactNames(): string[] {
+	if (!localStorage.getItem("unseenContactNames"))
+		setUnseenContactNames([]);
+	return JSON.parse(localStorage.getItem("unseenContactNames") as string);
+}
+
+export function removeUnseenContactName(contactName: string) {
+	const unseenContactNames: string[] = getAllUnseenContactNames();
+	setUnseenContactNames(removeItemFromArray(unseenContactNames, contactName));
+}
+
+export function removeAllUnseenContactNames() {
+	localStorage.removeItem("unseenContactNames");
+}
+
+function setUnseenContactNames(unseenContactNames: string[]) {
+	localStorage.setItem("unseenContactNames", JSON.stringify(unseenContactNames));
+}
+
+function removeItemFromArray(array: string[], item: string) {
+	return array.filter(value => value !== item);
+}
