e6d0ee28f5f291daa7305357465ea92093a41939
Neemias Santos <neemiassgc@outlook.com>
2024-07-05 23:49:18 -0300

    refactor: move the validateContact function into the misc module

--- src/app/lib/misc.ts
+++ src/app/lib/misc.ts
@@ -1,4 +1,4 @@
-import { Contact, ErrorType, ViolationError } from "./types";
+import { Contact, ErrorType, ShortContact, ViolationError } from "./types";
 
 export function toKeys(input: object): string[] {
   const keys: string[] = [];
@@ -45,4 +45,28 @@ export function iterator(size: number): any[] {
 export function loginIfTokenIsExpired(error: ErrorType) {
   if (error && error.message === "The access token expired and a refresh token is not available. The user will need to sign in again.")
     window.location.assign("/api/auth/login");
+}
+
+export function validateContact({ name, phoneLabel, phoneValue }: ShortContact): void {
+  const violations: {
+    [index: string]: string[],
+    name: string[],
+    phoneLabel: string[],
+    phoneValue: string[]
+  } = {
+    name: [],
+    phoneLabel: [],
+    phoneValue: [],
+  };
+
+  if (name.length < 3) violations.name.push("name must be at least 3 characters long");
+  if (name.length > 54) violations.name.push("name must be a maximum of 54 characters long");
+  if (phoneLabel.length < 3) violations.phoneLabel.push("label must be at least 3 characters long");
+  if (phoneLabel.length > 10) violations.phoneLabel.push("label must be a maximum of 10 characters long");
+  if (phoneLabel.includes(" ")) violations.phoneLabel.push("label must not have blank spaces");
+  if (!/^\+[0-9 ]{10,15}$|^\+?[0-9 ]{11,15}$/.test(phoneValue)) violations.phoneValue.push("phone number is not valid");
+  
+  for (let key in violations)
+    if (violations[key].length > 0)
+      throw new ViolationError(JSON.stringify(violations));
 }
--- src/app/lib/net.ts
+++ src/app/lib/net.ts
@@ -1,5 +1,5 @@
-import { isApplicationJson } from "./misc";
-import { Contact, ErrorType, ShortContact, ViolationError } from "./types";
+import { isApplicationJson, validateContact } from "./misc";
+import { Contact, ErrorType, ShortContact } from "./types";
 
 function getUrl(path: string): string {
   return window.location.origin + path;
@@ -28,30 +28,6 @@ export async function createNewContact(contact: ShortContact) {
   }
 
   return poster(getUrl("/api/contacts"), header, body);
-}
-
-function validateContact({ name, phoneLabel, phoneValue }: ShortContact): void {
-  const violations: {
-    [index: string]: string[],
-    name: string[],
-    phoneLabel: string[],
-    phoneValue: string[]
-  } = {
-    name: [],
-    phoneLabel: [],
-    phoneValue: [],
-  };
-
-  if (name.length < 3) violations.name.push("name must be at least 3 characters long");
-  if (name.length > 54) violations.name.push("name must be a maximum of 54 characters long");
-  if (phoneLabel.length < 3) violations.phoneLabel.push("label must be at least 3 characters long");
-  if (phoneLabel.length > 10) violations.phoneLabel.push("label must be a maximum of 10 characters long");
-  if (phoneLabel.includes(" ")) violations.phoneLabel.push("label must not have blank spaces");
-  if (!/^\+[0-9 ]{10,15}$|^\+?[0-9 ]{11,15}$/.test(phoneValue)) violations.phoneValue.push("phone number is not valid");
-  
-  for (let key in violations)
-    if (violations[key].length > 0)
-      throw new ViolationError(JSON.stringify(violations));
 }
 
 async function poster(url: string, headers: HeadersInit, body: string): Promise<null> {
