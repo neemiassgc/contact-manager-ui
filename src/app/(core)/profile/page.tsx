ae8fc03bd16f92097511dbe4e878be8c5eeec01a
Neemias Santos <neemiassgc@outlook.com>
2024-07-22 00:41:10 -0300

    refactor(Body): redesign the whole body of content of the page

--- src/app/(core)/profile/page.tsx
+++ src/app/(core)/profile/page.tsx
@@ -2,35 +2,30 @@
 
 import {
   Avatar, Box, TextField, Tooltip, IconButton,
-  Accordion, AccordionSummary, AccordionDetails, ToggleButtonGroup,
-  ToggleButton,
-  Breadcrumbs,
-  Typography,
-  DialogContent,
-  DialogTitle,
-  Dialog
+  Breadcrumbs, Typography, DialogContent, DialogTitle, Dialog,
+  ListSubheader, List, ListItem, ListItemButton, ListItemAvatar,
+  ListItemText, Button, Divider
 } from "@mui/material";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import EmailIcon from '@mui/icons-material/Email';
 import PhoneIcon from '@mui/icons-material/Phone';
-import HomeIcon from '@mui/icons-material/Home';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import DomainIcon from '@mui/icons-material/Domain';
+import ClearIcon from '@mui/icons-material/Clear';
+import AddIcon from '@mui/icons-material/AddToPhotos';
 import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import { toKeys } from "../../lib/misc";
-import { Contact, AddressType, Run } from "../../lib/types";
+import { formatAddress, locateCountryFlag, toKeys } from "../../lib/misc";
+import { Contact, AddressType, Run, StringType } from "../../lib/types";
 import { useState } from "react";
 import { getSelectedContact } from "../../lib/storage";
-import { bg, text, paint, border, color} from "../../lib/colors";
+import { bg, text, paint, border } from "../../lib/colors";
 import Link from "next/link";
 
 export default function Page() {
   const selectedContact: Contact = getSelectedContact() as Contact;
 
   return (
-    <Box className="w-11/12 md:w-1/2 mx-auto mt-10 mb-5">
+    <Box className="w-full sm:w-9/12 md:w-10/12 xl:w-8/12 mx-auto mt-10 mb-5">
       <Breadcrumbs sx={paint(text("on-surface"))}>
         <Link href="/">Home</Link>
         <Typography>Profile</Typography>
@@ -50,6 +45,7 @@ function Header(props: {contactName: string}) {
         width: 100, height: 100, fontSize: 50,
         ...paint(bg("primary"), text("on-primary"))
       }}
+      src="/flag.svg"
     >{props.contactName[0]}</Avatar>
     <Box className="ml-5 mt-1">
       <span style={paint(text("on-surface"))}>{props.contactName}</span>
@@ -68,159 +64,116 @@ function Header(props: {contactName: string}) {
   </Box>
 }
 
-function Body({ contact }: {contact: Contact}) {
-  const [creatingPhone, setCreatingPhone] = useState(false);
-  const [creatingEmail, setCreatingEmail] = useState(false);
-  const [expanded, setExpanded] = useState([true, true, true])
-
-  const handleExpanded = (index: number) => (_:any, value: boolean) => {
-    const copy: boolean[] = [...expanded];
-    copy[index] = value;
-    setExpanded(copy);
-  }
-
-  const sx: object = {
-    border: 1,
-    boxShadow: 0,
-    borderLeft: 3,
-    ...paint(bg("surface"), text("on-surface"), border("outline-variant")),
-    borderLeftColor: color("tertiary")
-  };
-
-  return <>
-    <Accordion sx={sx} expanded={expanded[0]} onChange={handleExpanded(0)}>
-      <AccordionSummary expandIcon={<KeyboardArrowDownIcon/>}>
-        <PhoneIcon className="mr-2"/> Phone
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box className="w-full justify-start flex gap-2 flex-wrap">
-          {
-            toKeys(contact.phoneNumbers).map((prop, index) => {
-                return <ContentBox key={index} label={prop} content={contact.phoneNumbers[prop]} deleteHandle={() => null}/>
-            })
-          }
-          <CreationPlaceholder onClick={() => setCreatingPhone(true)}/>
+function Body(props: { contact: Contact }) {
+  return (
+    <Box className="w-full mt-5">
+      <Box className="w-full flex flex-col md:flex-row justify-center gap-10 md:gap-5 lg:gap-10 xl:gap-20">
+        <Box className="flex-1">
+          <ListCard
+            titleIcon={<PhoneIcon className="mr-2"/>}
+            cardTitle="Phone Numbers"
+            addButtonTitle="Add Phone Number"
+            content={props.contact.phoneNumbers}
+          />
+          
         </Box>
-      </AccordionDetails>
-    </Accordion>
-    <Accordion sx={sx} expanded={expanded[1]} onChange={handleExpanded(1)}>
-      <AccordionSummary expandIcon={<KeyboardArrowDownIcon/>}>
-        <EmailIcon className="mr-2"/> Email
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box className="w-full justify-start flex gap-2 flex-wrap">
-          {
-            toKeys(contact.emails).map((prop, index) => {
-                return <ContentBox key={index} label={prop} content={contact.emails[prop]} deleteHandle={() => null}/>
-            })
-          }
-          <CreationPlaceholder onClick={() => setCreatingEmail(true)}/>
+        <Box className="flex-1 rounded-xl border">
+          <ListCard
+            titleIcon={<AlternateEmailIcon className="mr-2"/>}
+            cardTitle="Emails"
+            addButtonTitle="Add Email"
+            content={props.contact.emails}
+          />
         </Box>
-      </AccordionDetails>
-    </Accordion>
-    <Accordion sx={sx} expanded={expanded[2]} onChange={handleExpanded(2)}>
-      <AccordionSummary expandIcon={<KeyboardArrowDownIcon/>}>
-        <HomeIcon className="mr-2"/> Address
-      </AccordionSummary>
-      <AccordionDetails>
-        <AddressBoard addresses={contact.addresses}/>
-      </AccordionDetails>
-    </Accordion>
-    <PromptModal open={creatingPhone} title="Create a new phone" handleClose={() => setCreatingPhone(false)} handleSave={()=>{}}/>
-    <PromptModal open={creatingEmail} title="Create a new email" handleClose={() => setCreatingEmail(false)} handleSave={()=>{}}/>
-  </>
+      </Box>
+      <Box className="w-full md:w-5/6 mx-auto mt-10 rounded-xl border">
+        <ListCard
+          titleIcon={<DomainIcon className="mr-2"/>}
+          cardTitle="Addresses"
+          addButtonTitle="Add Address"
+          content={props.contact.addresses}
+        />
+      </Box>
+    </Box>
+  )
 }
 
-function AddressBoard(props: { addresses: AddressType}) {
-  const [alignment, setAlignment] = useState(0);
-  const [creating, setCreating] = useState(false);
-
-  function handleAlignment(_: any, value: number): void {
-    if (value !== null) setAlignment(value);
-  }
-
-  const addressKeys: string[] = toKeys(props.addresses);
-
+function ListCard(props: { titleIcon: React.ReactElement, addButtonTitle: string, cardTitle: string, content: StringType | AddressType }) {
   return (
-    <Box className="w-full p-3 border rounded-lg" sx={border("outline-variant")}>
-      <Box className="w-full mb-5 flex justify-evenly">
-        <Tooltip title="Delete Address">
-          <IconButton sx={text("secondary")}><DeleteIcon sx={{fontSize: 27}}/></IconButton>
-        </Tooltip>
-        <ToggleButtonGroup 
-          sx={{
-            ...paint(bg("secondary-container"), text("on-secondary-container")),
-            "& .MuiToggleButton-root.Mui-selected": {
-              ...paint(bg("secondary"), text("on-secondary"))
-            }
-          }}
-          value={alignment} exclusive
-          onChange={handleAlignment} size="small"
-        >
-          {
-            addressKeys.map((item, index) => <ToggleButton sx={{color: "inherit"}} key={index} value={index}>{item}</ToggleButton>)
-          }
-        </ToggleButtonGroup>
-        <Tooltip title="Create New Address">
-          <IconButton sx={text("secondary")} onClick={() => setCreating(true)}><AddBoxRoundedIcon sx={{fontSize: 27}}/></IconButton>
-        </Tooltip>
-      </Box>
+    <Box className="w-full p-9 rounded-2xl" sx={paint(bg("surface-container-high"))}>
+      <List
+        className="rounded-b-xl border-b-2"
+        sx={paint(bg("surface"), text("on-surface"), bg("surface-container-high"))}
+        subheader={
+        <ListSubheader
+          component="div" className="rounded-t-xl text-center"
+          sx={paint(bg("secondary"), text("on-secondary"))}>
+          <Box className="flex-shrink mr-2">
+            {props.titleIcon}
+            <span>{props.cardTitle}</span>
+          </Box>
+        </ListSubheader>
+      }>
         {
-          addressKeys.map((label, i) => {
+          toKeys(props.content).map((key, index, list) => {
             return (
-              <Box key={i} className={"flex gap-2 flex-wrap justify-center lg:justify-start "+(alignment !== i ? "hidden" : "")}>
+              <>
+                <ListItem disableGutters
+                  className="border-x-2"
+                  sx={bg("surface")}
+                  key={key}
+                  secondaryAction={
+                    <IconButton>
+                      <ClearIcon sx={text("on-surface")}/>
+                    </IconButton>
+                  }
+                >
+                  <ListItemButton>
+                    {
+                      typeof props.content[key] === "object" &&
+                      <ListItemAvatar>
+                        <Avatar src={locateCountryFlag(props.content[key].country)}>
+                          {props.content[key].country[0]}
+                        </Avatar>
+                      </ListItemAvatar>
+                    }
+                    <ListItemText
+                      primary={
+                        typeof props.content[key] === "object" ? formatAddress(props.content[key]) : props.content[key]
+                      }
+                      secondary={key}
+                    />
+                  </ListItemButton>
+                </ListItem>
                 {
-                  toKeys(props.addresses[label]).map((prop, j) => {
-                    return <ContentBox key={j} label={prop} content={props.addresses[label][prop]}/>
-                  })
+                  index !== list.length - 1 && <Divider variant="middle"/> 
                 }
-              </Box>
+              </>
             )
           })
         }
-      <PromptModal
-        open={creating}
-        title="Create a new address"
-        fieldNames={toKeys(props.addresses[addressKeys[0]])}
-        handleClose={() => setCreating(false)}
-        handleSave={()=>{}}
-      />
+      </List>
+      <AddButton title={props.addButtonTitle}/>
     </Box>
   )
 }
 
-function ContentBox(props: {label: string, content: string, deleteHandle?: Run}) {
-  const classes: string[] = [
-    "w-fit", "flex", "flex-col", "gap-0", "justify-center",
-    "border", "rounded-lg", "p-2", "hover:cursor-pointer"
-  ]
+function AddButton({ title }: { title: string }) {
   return (
-  <Box className={classes.join(" ")}
-    sx={paint(bg("secondary"), text("on-secondary"))}
-  >
-    <span style={{color: "inherit", opacity: 0.9}} className="text-center sm:text-start text-sm">{props.label}</span>
-    <Box className="flex gap-0 flex-wrap justify-center sm:justify-normal">
-      <span style={{color: "inherit"}} className="text-center h-fit my-auto">{props.content}</span>
-      {
-      props.deleteHandle &&
-       <Tooltip title={"Delete "+props.label}>
-        <IconButton sx={{color: "inherit"}} onClick={props.deleteHandle} size="small"><DeleteIcon/></IconButton> 
-       </Tooltip>
-      }
-    </Box>
-  </Box>
-  )
-}
-
-function CreationPlaceholder(props: {onClick: Run}) {
-  return (
-    <Box
-      onClick={props.onClick}
-      className="border-4 border-dotted rounded-lg hover:cursor-pointer flex flex-col justify-center p-3"
-      sx={border("outline-variant")}
+    <Button
+      variant="contained"
+      className="w-fit float-end capitalize"
+      size="small"
+      disableElevation
+      sx={{
+        ...paint(bg("primary"), text("on-primary")),
+        "&:hover": paint(bg("primary"), text("on-primary")),
+        borderRadius: 2
+      }}
+      startIcon={<AddIcon/>}
     >
-      <AddBoxRoundedIcon fontSize="large" sx={text("secondary")}/>
-    </Box>
+      {title}
+    </Button>
   )
 }
 
