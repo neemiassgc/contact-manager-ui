917251bec931214c3f47f569cb761e2d556e3287
Neemias Santos <neemiassgc@outlook.com>
2024-10-02 12:27:32 -0300

    chore: redirect the user to / if authorization is denied

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,3 +1,25 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback,  NextAppRouterHandler } from '@auth0/nextjs-auth0';
+import { NextResponse } from 'next/server';
 
-export const GET = handleAuth();
+function parse(url: string) {
+  const params = url.split("?")[1].split("&");
+  const queryParams = params.reduce((prev, curr) => {
+    const param = curr.split("=");
+    return {
+      ...prev,
+      [param[0]]: param[1]
+    }
+  }, {});
+  return queryParams;
+}
+
+const callbackHandler: NextAppRouterHandler = async (req, ctx) => {
+  const params = parse(req.url);
+  if ("error" in params && params.error === "access_denied")
+    return new NextResponse(null, { status: 301, headers: { "Location": "/" }})
+  return await handleCallback(req, ctx);
+}
+
+export const GET = handleAuth({
+  callback: callbackHandler
+});
