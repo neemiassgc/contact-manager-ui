f684b6de9ffb91d8025e3879c7c668ff672b277f
Neemias Santos <neemiassgc@outlook.com>
2025-02-13 00:54:16 -0300

    feat(profile): Create and export the BreadcrumbsNavigator component

--- src/app/subframe/profile/[id]/components.tsx
+++ src/app/subframe/profile/[id]/components.tsx
@@ -10,3 +10,31 @@ import { Address, Contact } from "@/app/lib/types";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { DropdownMenu } from "@/subframe/components/DropdownMenu";
+
+export function BreadcrumbsNavigator() {
+  const nextRouter = useRouter();
+
+  const redirectToHome = () => nextRouter.push("/subframe");
+
+  return (
+    <div className="flex w-full items-center gap-4 px-4 py-4">
+      <div className="flex items-center gap-1">
+        <IconButton
+          size="small"
+          icon="FeatherChevronLeft"
+          onClick={redirectToHome}
+        />
+        <IconButton
+          disabled={true}
+          size="small"
+          icon="FeatherChevronRight"
+        />
+      </div>
+      <Breadcrumbs>
+        <Breadcrumbs.Item  onClick={redirectToHome}>Home</Breadcrumbs.Item>
+        <Breadcrumbs.Divider />
+        <Breadcrumbs.Item active={true}>profile</Breadcrumbs.Item>
+      </Breadcrumbs>
+    </div>
+  )
+}
