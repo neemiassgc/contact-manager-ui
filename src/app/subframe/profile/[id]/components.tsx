ce060971edc363a1eb33dff14d7c384ce223dd85
Neemias Santos <neemiassgc@outlook.com>
2025-02-13 00:56:44 -0300

    feat(profile): Implement contact content component with tabs

--- src/app/subframe/profile/[id]/components.tsx
+++ src/app/subframe/profile/[id]/components.tsx
@@ -38,3 +38,41 @@ export function BreadcrumbsNavigator() {
     </div>
   )
 }
+
+export function ContactContent(props: { contact: Contact }) {
+  const [tabActive, setTabActive] = useState<"phone" | "email" | "address">("phone");
+
+  const tabOptions = {
+    "phone": (
+      <ListTab
+        content={props.contact.phoneNumbers}
+        variant="phone"
+        onAddButtonClick={()=>{}}
+        onDeleteWithConfirmation={()=>{}}
+      />),
+    "email": (
+      <ListTab
+        content={props.contact.emails}
+        variant="email"
+        onAddButtonClick={()=>{}}
+        onDeleteWithConfirmation={()=>{}}
+      />),
+    "address": <AddressTab addresses={props.contact.addresses} />
+  }
+
+  return (
+    <div className="flex w-full flex-col items-start gap-6">
+      <Tabs>
+        <Tabs.Item active={tabActive === "phone"} onClick={() => setTabActive("phone")} icon="FeatherPhone">
+          Phone numbers
+        </Tabs.Item>
+        <Tabs.Item active={tabActive === "email"} onClick={() => setTabActive("email")} icon="FeatherMail">Emails</Tabs.Item>
+        <Tabs.Item active={tabActive === "address"} onClick={() => setTabActive("address")} icon="FeatherMapPin">Addresses</Tabs.Item>
+      </Tabs>
+      <div className="flex w-full flex-col items-start">
+        {tabOptions[tabActive]}
+      </div>
+    </div>
+  )
+}
+}
