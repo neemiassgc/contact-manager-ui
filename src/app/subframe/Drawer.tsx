d2cd385d06d24f468a8a4af55176dbcb87f75080
Neemias Santos <neemiassgc@outlook.com>
2025-02-19 01:10:50 -0300

    chore: add insertAt utility function to insert value at specific index in array

--- src/app/subframe/Drawer.tsx
+++ src/app/subframe/Drawer.tsx
@@ -252,3 +252,11 @@ function RemoveButton(props: {onClick: () => void}) {
     />
   )
 }
+
+function insertAt<T>(array: T[], index: number, value: T): T[] {
+  return [
+    ...array.slice(0, index),
+    value,
+    ...array.slice(index)
+  ]
+};
