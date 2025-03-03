b79de71a51812666197e752564f31c3fa2a06e97
Neemias Santos <neemiassgc@outlook.com>
2025-03-02 22:28:40 -0300

    chore: Remove no longer necessary files

--- src/app/lib/AlertContext.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-import { ShowAlertFunc } from "./types";
-
-const AlertContext = createContext<ShowAlertFunc>((msg, severity) => {});
-export default AlertContext;
