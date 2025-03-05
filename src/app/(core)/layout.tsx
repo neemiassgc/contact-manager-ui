8fccef80c3b10f0e914ebbb008b84764d8afc6ea
Neemias Santos <neemiassgc@outlook.com>
2025-03-04 23:40:59 -0300

    refactor(layout): Replace DefaultPageLayout with ScreenLoader

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,5 +1,5 @@
-import DefaultPageLayout from "@/subframe/layouts/PageLayout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import ScreenLoader from "./components/ScreenLoader";
 
 export default function Layout({
   children,
@@ -8,9 +8,7 @@ export default function Layout({
 }>) {
   return (
     <UserProvider>
-      <DefaultPageLayout>
-        {children}
-      </DefaultPageLayout>
+      <ScreenLoader>{children}</ScreenLoader>
     </UserProvider>
   )
 }
