a782bcbf2a686ba224af6097fc8a11c8a96ca50f
Neemias Santos <neemiassgc@outlook.com>
2025-08-17 23:52:15 -0300

    feat(PageLayout): Implement responsive topbar for mobile view

--- src/subframe/layouts/PageLayout.tsx
+++ src/subframe/layouts/PageLayout.tsx
@@ -7,6 +7,7 @@ import { Tooltip } from "@/subframe/components/Tooltip";
 import { IconButton } from "@/subframe/components/IconButton";
 import { Avatar } from "@/subframe/components/Avatar";
 import Image from "next/image";
+import { TopbarWithLeftNav } from "../components/TopbarWithLeftNav";
 
 interface DefaultPageLayoutRootProps
   extends React.HTMLAttributes<HTMLDivElement> {
@@ -23,89 +24,144 @@ const DefaultPageLayoutRoot = React.forwardRef<
   { children, className, ...otherProps }: DefaultPageLayoutRootProps,
   ref
 ) {
-  const [loading, setLoading] = useState(false);
+  const [mobile, setMobile] = useState(true);
+
+  setTimeout(() => setMobile(!mobile), 3000)
+
+  const logoImage = (
+    <Image
+      width={32} height={32}
+      className="flex-none object-cover"
+      src="/logo.svg"
+      alt="project logo"
+    />
+  )
 
   return (
     <div
       className={SubframeCore.twClassNames(
-        "flex h-screen w-full items-start",
+        `flex h-screen w-full ${mobile ? "flex-col items-center" : "items-start"}`,
         className
       )}
       ref={ref as any}
       {...otherProps}
     >
-      <SidebarRailWithIcons
-        header={
-          <div className="flex flex-col items-center justify-center gap-2 px-1 py-1">
-            <Image
-              width={32} height={32}
-              className="flex-none object-cover"
-              src="/logo.svg"
-              alt="project logo"
-            />
-          </div>
-        }
-        footer={
-          <div className="flex flex-col items-center justify-end gap-4 px-1 py-1">
-            <SubframeCore.Tooltip.Provider>
-              <SubframeCore.Tooltip.Root>
-                <SubframeCore.Tooltip.Trigger asChild={true}>
-                  <IconButton
-                    variant="brand-secondary"
-                    icon="FeatherLogOut"
-                    loading={loading}
-                    disabled={loading}
-                    onClick={() => {
-                      setLoading(true);
-                      window.location.assign("/api/auth/logout");
-                    }}
-                  />
-                </SubframeCore.Tooltip.Trigger>
-                <SubframeCore.Tooltip.Portal>
-                  <SubframeCore.Tooltip.Content
-                    side="right"
-                    align="center"
-                    sideOffset={4}
-                    asChild={true}
-                  >
-                    <Tooltip>{loading ? "Leaving..." : "Log Out"}</Tooltip>
-                  </SubframeCore.Tooltip.Content>
-                </SubframeCore.Tooltip.Portal>
-              </SubframeCore.Tooltip.Root>
-            </SubframeCore.Tooltip.Provider>
-            <SubframeCore.Tooltip.Provider>
-            <SubframeCore.Tooltip.Root>
-                <SubframeCore.Tooltip.Trigger asChild={true}>
-                  <Avatar image={otherProps.picture}>
-                    A
-                  </Avatar>
-                </SubframeCore.Tooltip.Trigger>
-                <SubframeCore.Tooltip.Portal>
-                  <SubframeCore.Tooltip.Content
-                    side="right"
-                    align="start"
-                    sideOffset={4}
-                    asChild={true}
-                  >
-                    <Tooltip>{otherProps.username}</Tooltip>
-                  </SubframeCore.Tooltip.Content>
-                </SubframeCore.Tooltip.Portal>
-              </SubframeCore.Tooltip.Root>
-            </SubframeCore.Tooltip.Provider>
-          </div>
-        }
-      >
-        <SidebarRailWithIcons.NavItem icon="FeatherHome" selected={true}>
-          Home
-        </SidebarRailWithIcons.NavItem>
-      </SidebarRailWithIcons>
+      { mobile ?
+        <TopbarWithLeftNav
+          leftSlot={
+            <div className="flex gap-6">
+              {logoImage}
+              <HomeButton/>
+            </div>
+          }
+          rightSlot={<ProfileButtons username={otherProps.username} picture={otherProps.picture}/>}
+        /> :
+        <SidebarRailWithIcons
+          header={
+            <div className="flex flex-col items-center justify-center gap-2 px-1 py-1">
+              <Image
+                width={32} height={32}
+                className="flex-none object-cover"
+                src="/logo.svg"
+                alt="project logo"
+              />
+            </div>
+          }
+          footer={<ProfileButtons flexCol username={otherProps.username} picture={otherProps.picture}/>}
+        >
+          <HomeButton/>
+        </SidebarRailWithIcons>
+      }
       {children ? (
-        <div className="flex grow shrink-0 basis-0 flex-col items-start gap-2 self-stretch overflow-y-auto bg-default-background">
+        <div
+        className={`flex w-full grow shrink-0 basis-0 flex-col items-start overflow-y-auto bg-default-background ${mobile ? "w-full gap-4" : "gap-2 self-stretch"}`}>
           {children}
         </div>
       ) : null}
     </div>
   );
 });
+
+function ProfileButtons(props: { username: string, picture: string, flexCol?: boolean }) {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className={`flex ${props.flexCol ? "flex-col" : ""} justify-end gap-4 px-1 py-1`}>
+      <SubframeCore.Tooltip.Provider>
+        <SubframeCore.Tooltip.Root>
+          <SubframeCore.Tooltip.Trigger asChild={true}>
+            <IconButton
+              variant="brand-secondary"
+              icon="FeatherLogOut"
+              loading={loading}
+              disabled={loading}
+              onClick={() => {
+                setLoading(true);
+                window.location.assign("/api/auth/logout");
+              }}
+            />
+          </SubframeCore.Tooltip.Trigger>
+          <SubframeCore.Tooltip.Portal>
+            <SubframeCore.Tooltip.Content
+              side="right"
+              align="center"
+              sideOffset={4}
+              asChild={true}
+            >
+              <Tooltip>{loading ? "Leaving..." : "Log Out"}</Tooltip>
+            </SubframeCore.Tooltip.Content>
+          </SubframeCore.Tooltip.Portal>
+        </SubframeCore.Tooltip.Root>
+      </SubframeCore.Tooltip.Provider>
+      <SubframeCore.Tooltip.Provider>
+      <SubframeCore.Tooltip.Root>
+          <SubframeCore.Tooltip.Trigger asChild={true}>
+            <Avatar image={props.picture}>
+              A
+            </Avatar>
+          </SubframeCore.Tooltip.Trigger>
+          <SubframeCore.Tooltip.Portal>
+            <SubframeCore.Tooltip.Content
+              side="right"
+              align="start"
+              sideOffset={4}
+              asChild={true}
+            >
+              <Tooltip>{props.username}</Tooltip>
+            </SubframeCore.Tooltip.Content>
+          </SubframeCore.Tooltip.Portal>
+        </SubframeCore.Tooltip.Root>
+      </SubframeCore.Tooltip.Provider>
+    </div>
+  )
+}
+
+function HomeButton() {
+  return (
+    <SubframeCore.Tooltip.Provider>
+      <SubframeCore.Tooltip.Root>
+        <SubframeCore.Tooltip.Trigger asChild={true}>
+          <div className="flex items-center gap-2">
+            <IconButton
+              variant="brand-secondary"
+              icon="FeatherHome"
+              onClick={(event: React.MouseEvent<HTMLButtonElement>) => {}}
+            />
+          </div>
+        </SubframeCore.Tooltip.Trigger>
+        <SubframeCore.Tooltip.Portal>
+          <SubframeCore.Tooltip.Content
+            side="bottom"
+            align="center"
+            sideOffset={4}
+            asChild={true}
+          >
+            <Tooltip>Home</Tooltip>
+          </SubframeCore.Tooltip.Content>
+        </SubframeCore.Tooltip.Portal>
+      </SubframeCore.Tooltip.Root>
+    </SubframeCore.Tooltip.Provider>
+  )
+}
 
 export default DefaultPageLayoutRoot;
